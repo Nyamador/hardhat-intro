32d704ce65f797be251b64ac51f72d132afce8b7
Desmond Nyamador <dselasi4u@gmail.com>
2022-02-12 13:50:55 +0000

    Added wallet connection on page load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,57 @@ import { ethers } from "ethers";
 import "./App.css";
 
 export default function App() {
+  const [currentAcc, setCurrentAcc] = React.useState("");
+
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("Ethereum is in scope", ethereum);
+      }
+
+      //Check if we have access to users wallet
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAcc(currentAcc);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    isWalletConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAcc(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const wave = () => {};
 
   return (
