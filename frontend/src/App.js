8646723e787b15f0e95a0a886959bc0090bd10e8
Desmond Nyamador <dselasi4u@gmail.com>
2022-02-12 11:53:24 +0000

    Added UI

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import "./App.css";
+
+export default function App() {
+  const wave = () => {};
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">
+          Hi there! Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
